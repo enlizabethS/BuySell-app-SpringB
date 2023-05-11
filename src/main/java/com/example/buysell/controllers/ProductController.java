2cf245b09ec90ba42d79f69f794e62076bbe4423
endorphinchik <elisaveta.stepura@mail.ru>
2023-05-11 13:49:07 +0300

    new

--- src/main/java/com/example/buysell/controllers/ProductController.java
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,8 +19,8 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/")
-    public String products(Model model) {
-        model.addAttribute("products",productService.listProducts());
+    public String products(@RequestParam(name = "title", required = false)String title, Model model) {
+        model.addAttribute("products",productService.listProducts(title));
         return "products";
     }
     @GetMapping("/product/{id}")
@@ -26,8 +30,9 @@ public class ProductController {
     }
 
     @PostMapping("product/create")
-    public String createProduct(Product product){
-        productService.saveProduct(product);
+    public String createProduct(@RequestParam("file1") MultipartFile file1,@RequestParam("file2") MultipartFile file2,@RequestParam("file3") MultipartFile file3,
+                                Product product) throws IOException {
+        productService.saveProduct(product,file1,file2,file3);
         return "redirect:/";
     }
     @PostMapping("/product/delete/{id}")
--- src/main/java/com/example/buysell/services/ProductService.java
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -1,33 +1,65 @@
 package com.example.buysell.services;
 
+import com.example.buysell.models.Image;
 import com.example.buysell.models.Product;
+import com.example.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
-    {
-        products.add(new Product(++ID,"PlayStation 5 ","Simple description",55000,"Moscow","Nikolay"));
-        products.add(new Product(++ID,"Nintendo ","Simple description",35000,"Moscow","Anton"));
+    private final ProductRepository productRepository;
+    public List<Product> listProducts(String title){
+        if(title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
-    public List<Product> listProducts(){ return products; }
 
-    public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
+    public void saveProduct(Product product, MultipartFile file1,MultipartFile file2,MultipartFile file3) throws IOException {
+        Image image1;
+        Image image2;
+        Image image3;
+        if(file1.getSize() !=0){
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImageToProduct(image1);
+        }
+        if(file2.getSize() !=0){
+            image2 = toImageEntity(file2);
+            product.addImageToProduct(image2);
+        }
+        if(file3.getSize() !=0){
+            image3 = toImageEntity(file3);
+            product.addImageToProduct(image3);
+        }
+        log.info("Saving new Product. Title: {}; Author: {} ",product.getTitle(),product.getAuthor());
+        Product productFromDb = productRepository.save(product);
+        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
+        productRepository.save(product);
     }
+
+    private Image toImageEntity(MultipartFile file) throws IOException {
+       Image image = new Image();
+       image.setName(file.getName());
+       image.setOriginalFileName(file.getOriginalFilename());
+       image.setContentType(file.getContentType());
+       image.setSize(file.getSize());
+       image.setBytes(file.getBytes());
+        return image;
+    }
+
     public void deleteProduct(Long id){
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for(Product product :products){
-            if(product.getId().equals(id)) return product;
-        }
-        return null;
-    }
+     return  productRepository.findById(id).orElse(null);
+}
 }
