05feb999a3ec0bdff374861ddda52e1ee72ca8f4
endorphinchik <elisaveta.stepura@mail.ru>
2023-05-12 12:39:15 +0300

    new

---
+++ src/main/java/com/example/buysell/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.example.buysell.controllers;
+
+import com.example.buysell.models.User;
+import com.example.buysell.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+    @PostMapping("/registration")
+    public String createUser(User user, Model model){
+        if(!userService.createUser(user)){
+            model.addAttribute("errorMessage","A person with such an email : " + user.getEmail() + " already exists");
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+    @GetMapping("/hello")
+    public String securityUrl(){
+        return "hello";
+    }
+
+}
---
+++ src/main/java/com/example/buysell/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.buysell.repositories;
+
+import com.example.buysell.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User,Long> {
+    User findByEmail(String email);
+
+}
